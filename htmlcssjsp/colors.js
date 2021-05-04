ddf27009018f4ee56cef50e1dfb7bd383dc6b60d
devsungyeon <sungyeonlee1103@gmail.com>
2021-05-04 17:34:31 +0900

    done

--- htmlcssjsp/colors.js
+++ htmlcssjsp/colors.js
@@ -1,18 +1,21 @@
 var Links = {
     setColor:function (color) {
-        var i = 0;
-        var atagarr = document.querySelectorAll('a');
-        for (i = 0; i < atagarr.length; i++) {
-            atagarr[i].style.color = color;
-        }
+        // var i = 0;
+        // var alist = document.querySelectorAll('a');
+        // for (i = 0; i < alist.length; i++) {
+        //     alist[i].style.color = color;
+        // }
+        $('a').css('color', color);
     }
 }
 var Body = {
     setColor:function (color) {
-        document.querySelector('body').style.color = color;    
+        // document.querySelector('body').style.color = color;    
+        $('body').css('color', color);
     },
     setBackgroundColor:function (color) {
-        document.querySelector('body').style.backgroundColor = color;    
+        // document.querySelector('body').style.backgroundColor = color;    
+        $('body').css('backgroundColor', color);
     }
 }
 function nightDayHandler(self) {
